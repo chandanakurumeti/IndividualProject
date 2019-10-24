1a75151368d017b07bbe7f0257dfca75c8456f28
chandanakurumeti <chandanakurumeti27@gmail.com>
2019-10-24 17:34:32 +0200

    wrongly spelled class name(TaskOperationsTask) corrected to TaskOperationsTest

--- src/tasktests/TaskOperationsTest.java
+++ src/tasktests/TaskOperationsTest.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TaskOperationsTask {
+public class TaskOperationsTest {
 
     String datestring = "2018-09-10";
     TaskOperations tasks = new TaskOperations();
